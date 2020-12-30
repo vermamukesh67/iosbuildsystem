e5dbc28da794f2004066b07108fedc7e10458069
verma mukesh <vermamukesh67@gmail.com>
2020-12-30 17:46:21 +0800

    Release 0.0.1.

---
+++ iosbuildsystem/Pods/vermasdk/vermasdk/vermasdk.swift
@@ -0,0 +1,14 @@
+//
+//  vermasdk.swift
+//  iosbuildsystem
+//
+//  Created by verma mukesh on 30/12/20.
+//
+
+import Foundation
+
+open class Addition {
+    static public func add(firstName: String, lastName: String) -> String {
+        return firstName + " " + lastName
+    }
+}
